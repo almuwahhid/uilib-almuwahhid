33e49dcbd19c12a7015c21c60059873b7392d916
Al Muwahhid <muh.almuwahhid@gmail.com>
2019-04-13 18:13:58 +0700

    update third try

--- app/src/main/java/lib/almuwahhid/com/almuwahhidlib/MainActivity.kt
+++ app/src/main/java/lib/almuwahhid/com/almuwahhidlib/MainActivity.kt
@@ -2,11 +2,14 @@ package lib.almuwahhid.com.almuwahhidlib
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import lib.almuwahhid.Activity.ActivityGeneral
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ActivityGeneral() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
